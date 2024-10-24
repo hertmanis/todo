84e370e39bec3ff7ccee9879bd60f3d1265f5a6d
hertmanis <martinshertmanis4@gmail.com>
2024-10-24 18:57:27 +0300

    Changes

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    use HasFactory;
+
+    // This ensures that the 'title' field can be mass assigned
+    protected $fillable = ['title'];
+
+    // Optionally, you can add any relationships or custom methods below
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
+// Redirect root to todos index
 Route::get('/', function () {
     return redirect()->route('todos.index');
 });
 
+// Resource routes for todos
 Route::resource('todos', TodoController::class);
 
 // AJAX route for marking a todo as complete
 Route::post('todos/{todo}/complete', [TodoController::class, 'complete'])->name('todos.complete');
+
+// Authentication routes
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+    
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+});
+
+// Logout route
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout')->middleware('auth');
